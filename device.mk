1905e4bd953a95b78671b995f342122baef1bf59
Seplx <49388198+Seplx@users.noreply.github.com>
2021-01-17 21:24:16 +0100

    update rootdir files

--- device.mk
+++ device.mk
@@ -3,12 +3,12 @@ DEVICE_PATH := device/maxwest/sharkl2
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/rootdir/init.recovery.sc8830.rc:root/init.recovery.sc8830.rc \
-    $(DEVICE_PATH)/rootdir/ueventd.sc8830.rc:root/ueventd.sc8830.rc
+    $(DEVICE_PATH)/rootdir/init.recovery.sp9850ka_1h10_go.rc:root/init.recovery.sp9850ka_1h10_go.rc \
+    $(DEVICE_PATH)/rootdir/ueventd.sp9850ka_1h10_go.rc:root/ueventd.sp9850ka_1h10_go.rc
 
 # Stock Modules
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/rootdir/lib/modules/mali.ko:root/lib/modules/mali.ko \
-    $(DEVICE_PATH)/rootdir/lib/modules/marlin2_fm.ko:root/lib/modules/marlin2_fm.ko \
-    $(DEVICE_PATH)/rootdir/lib/modules/mtty.ko:root/lib/modules/mtty.ko \
-    $(DEVICE_PATH)/rootdir/lib/modules/sprdwl_ng.ko:root/lib/modules/sprdwl_ng.ko \
+    $(DEVICE_PATH)/prebuilt/lib/modules/mali.ko:root/lib/modules/mali.ko \
+    $(DEVICE_PATH)/prebuilt/lib/modules/marlin2_fm.ko:root/lib/modules/marlin2_fm.ko \
+    $(DEVICE_PATH)/prebuilt/lib/modules/mtty.ko:root/lib/modules/mtty.ko \
+    $(DEVICE_PATH)/prebuilt/lib/modules/sprdwl_ng.ko:root/lib/modules/sprdwl_ng.ko \
